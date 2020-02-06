af253f650fffd5a5c5511bb760f44b8a95ac0d58
AntonioDOrazio <antonio.dorazio9@outlook.it>
2020-02-06 12:00:42 +0100

    Solved issue #24

--- fixmycity/src/main/java/com/ispw/fixmycity/logic/bean/CompanyUserBean.java
+++ fixmycity/src/main/java/com/ispw/fixmycity/logic/bean/CompanyUserBean.java
@@ -1,5 +1,66 @@
 package com.ispw.fixmycity.logic.bean;
 
+import java.io.File;
+
 public class CompanyUserBean {
+	private String username;
+
+	private String password;
+
+	private String companyName;
+
+	private String category;
+
+	private String city;
+
+	private File image;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public File getImage() {
+		return image;
+	}
+
+	public void setImage(File image) {
+		this.image = image;
+	}
 
 }
