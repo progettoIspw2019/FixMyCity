1ad4b40f2002361015f87a7c0e8a28d12ba7afeb
adrianrob1 <44144508+adrianrob1@users.noreply.github.com>
2020-02-12 17:53:06 +0100

    solved #37

--- fixmycity/src/main/java/com/ispw/fixmycity/logic/view/MapController.java
+++ fixmycity/src/main/java/com/ispw/fixmycity/logic/view/MapController.java
@@ -1,6 +1,7 @@
 package com.ispw.fixmycity.logic.view;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import com.ispw.fixmycity.logic.bean.AddressBean;
@@ -12,10 +13,13 @@ import com.ispw.fixmycity.logic.view.javafx.MapBoundary;
 
 import javafx.scene.web.WebView;
 import net.java.html.boot.fx.FXBrowsers;
-import net.java.html.leaflet.Circle;
+import net.java.html.leaflet.Icon;
+import net.java.html.leaflet.IconOptions;
 import net.java.html.leaflet.LatLng;
 import net.java.html.leaflet.Map;
-import net.java.html.leaflet.PathOptions;
+import net.java.html.leaflet.Marker;
+import net.java.html.leaflet.MarkerOptions;
+import net.java.html.leaflet.Point;
 import net.java.html.leaflet.Popup;
 import net.java.html.leaflet.PopupOptions;
 import net.java.html.leaflet.TileLayer;
@@ -28,10 +32,13 @@ public class MapController {
 	private WebView mapView;
 	private MouseListener mouseListener;
 	private Map map;
+	private Icon iconComp;
+	private Icon iconComm;
+	private MarkerOptions markerOptComp;
+	private MarkerOptions markerOptComm;
 
 	public MapController(WebView mapView) {
 		this.setMapView(mapView);
-
 		mouseListener = (MouseEvent ev) -> {
 			BigDecimal latitude = BigDecimal.valueOf(ev.getLatLng().getLatitude());
 			BigDecimal longitude = BigDecimal.valueOf(ev.getLatLng().getLongitude());
@@ -54,17 +61,24 @@ public class MapController {
 			map = new Map("map");
 
 			TileLayer tileLayer = new TileLayer(
-					"http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=f1a0893344e045899384882196dacff3",
-					new TileLayerOptions().setAttribution(
-							"Map data &copy; <a href='http://www.thunderforest.com/opencyclemap/'>OpenCycleMap</a> contributors, "
-									+ "<a href='http://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, "
-									+ "Imagery © <a href='http://www.thunderforest.com/'>Thunderforest</a>")
-							.setMaxZoom(18).setId("eppleton.ia9c2p12"));
+					"https://1.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/256/png8"
+							+ "?apiKey=XUbEajSB94rqlnuoXCZkfMe_n3bUeeGghpHSejZkC50",
+					new TileLayerOptions()
+							.setAttribution(
+									"Map data &copy; <a href='https://developer.here.com/products/maps/'>HERE Api</a>")
+							.setMaxZoom(18).setId("fixmycity.ia9c2p12"));
 			map.addLayer(tileLayer);
 			// from here we just use the Leaflet API to show some stuff on the map
 			map.setView(new LatLng(41.902782, 12.496365), 13);
 
 			map.addMouseListener(MouseEvent.Type.CLICK, mouseListener);
+
+			this.iconComp = new Icon(new IconOptions("leaflet-0.7.2/images/marker-icon.png")
+					.setIconSize(new Point(25, 41)).setIconAnchor(new Point(12.5, 41)).setPopupAnchor(new Point(0, -20)));
+			this.iconComm = new Icon(new IconOptions("leaflet-0.7.2/images/marker-icon-community.png")
+					.setIconSize(new Point(25, 41)).setIconAnchor(new Point(12.5, 41)).setPopupAnchor(new Point(0, -20)));
+			this.markerOptComp = new MarkerOptions().setIcon(this.iconComp);
+			this.markerOptComm = new MarkerOptions().setIcon(this.iconComm);
 
 			for (ReportFilter filter : filters) {
 				if (filter == ReportFilter.ALL_COMMUNITY_REPORT)
@@ -79,9 +93,12 @@ public class MapController {
 		List<CompanyReportBeanView> reports = new SystemFacade().getCompanyReports();
 
 		for (var report : reports) {
-			map.addLayer(new Circle(new LatLng(report.getLatitude().floatValue(), report.getLongitude().floatValue()),
-					500, new PathOptions().setColor("cyan").setFillColor("#f05").setOpacity(0.5))
-							.bindPopup(report.getTitle()));
+			new Marker(new LatLng(report.getLatitude().floatValue(), report.getLongitude().floatValue()), markerOptComp)
+					.bindPopup(new Popup(new PopupOptions().setMaxWidth(200)).setContent("<b>" + report.getTitle() + "</b><br>" + 
+							report.getDescription() + "<br>" + 
+							"<i>submitted on " + new SimpleDateFormat("dd-MM-yyyy").format(report.getDateSubmission()) +
+							" by " + report.getSubmitter() + "</i>"))
+					.addTo(map);
 		}
 	}
 
@@ -89,9 +106,12 @@ public class MapController {
 		List<CommunityReportBeanView> reports = new SystemFacade().getCommunityReports();
 
 		for (var report : reports) {
-			map.addLayer(new Circle(new LatLng(report.getLatitude().floatValue(), report.getLongitude().floatValue()),
-					500, new PathOptions().setColor("red").setFillColor("#f03").setOpacity(0.5))
-							.bindPopup(report.getTitle()));
+			new Marker(new LatLng(report.getLatitude().floatValue(), report.getLongitude().floatValue()), markerOptComm)
+					.bindPopup(new Popup(new PopupOptions().setMaxWidth(200)).setContent("<b>" + report.getTitle() + "</b><br>" + 
+							report.getDescription() + "<br>" + 
+							"<i>submitted on " + new SimpleDateFormat("dd-MM-yyyy").format(report.getDateSubmission()) +
+							" by " + report.getSubmitter() + "</i>"))
+					.addTo(map);
 		}
 	}
 
