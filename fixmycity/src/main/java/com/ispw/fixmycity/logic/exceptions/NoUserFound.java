1a5e74bb704aed70cf176c9a4caacd8f3c3503b0
adrianrob1 <44144508+adrianrob1@users.noreply.github.com>
2020-02-14 13:53:50 +0100

    Solved #44.1 : refactory of exceptions package

---
+++ fixmycity/src/main/java/com/ispw/fixmycity/logic/exceptions/NoUserFound.java
@@ -0,0 +1,10 @@
+package com.ispw.fixmycity.logic.app.exceptions;
+
+public class NoUserFound extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9072580180933910518L;
+
+}
