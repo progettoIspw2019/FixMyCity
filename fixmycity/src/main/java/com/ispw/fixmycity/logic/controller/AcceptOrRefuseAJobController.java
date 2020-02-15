f1b33844666d4aba75d2dc24b91cf1c4f5b99dd5
fra199889 <55882910+fra199889@users.noreply.github.com>
2020-02-15 20:13:53 +0100

    #21 general refactory

--- fixmycity/src/main/java/com/ispw/fixmycity/logic/controller/AcceptOrRefuseAJobController.java
+++ fixmycity/src/main/java/com/ispw/fixmycity/logic/controller/AcceptOrRefuseAJobController.java
@@ -23,8 +23,8 @@ public class AcceptOrRefuseAJobController {
 		report.setStatus(status);
 	}
 
-	//AcceptOrRefuseJobForm form = new AcceptOrRefuseJobForm();
-
+	
+  
 	public byte[] uploadDocument() {
 		GridPane gridPane = new GridPane();
 		Window window = gridPane.getScene().getWindow();
@@ -47,37 +47,37 @@ public class AcceptOrRefuseAJobController {
 	}
 
 	public void jobCreation(AcceptOrRefuseJobBean bean) {
-//		Job job = new Job();
-//		job.setIdJob(bean.getIdJob());
-//		job.setStartDate(bean.getStartDate());
-//		job.setEndDate(bean.getEndDate());
-//		job.setJobInfo(bean.getDocument());
-//		job.setRelatedCompany(bean.getRelatedUser());
-//		job.setCompanyReport(bean.getCompanyReport());
-//		updateReportStatus(job.getCompanyReport(), "ReportAccepted");
-//		CompanyReportDAO dao = new CompanyReportDAO();
-//		dao.update(bean.getCompanyReport());
-//		job.getCompanyReport().notify();
-//		Stage stage = (Stage) form.submitAcceptButton.getScene().getWindow();
-//		stage.close();
+		Job job = new Job();
+		job.setIdJob(bean.getIdJob());
+		job.setStartDate(bean.getStartDate());
+		job.setEndDate(bean.getEndDate());
+		job.setJobInfo(bean.getDocument());
+		job.setRelatedCompany(bean.getRelatedUser());
+    	job.setCompanyReport(bean.getCompanyReport());
+		updateReportStatus(job.getCompanyReport(), "ReportAccepted");
+		CompanyReportDAO dao = new CompanyReportDAO();
+		dao.update(bean.getCompanyReport());
+	//	job.getCompanyReport().notify();
+		Stage stage = (Stage) AcceptOrRefuseJobForm.submitAcceptButton.getScene().getWindow();
+		stage.close();
 
 	}
 
 	public void refuseReport(AcceptOrRefuseJobBean bean) {
-//		CompanyReportDAO dao = new CompanyReportDAO();
-//		if (bean.getCompanyReport().getRefuseCounter() <= 3) {
-//			bean.getCompanyReport().setRefuseDescription(bean.getRefuseMotivation());
-//			updateReportStatus(bean.getCompanyReport(), "reporRefused");
-//			dao.update(bean.getCompanyReport());
-//
+		CompanyReportDAO dao = new CompanyReportDAO();
+		if (bean.getCompanyReport().getRefuseCounter() <= 3) {
+			bean.getCompanyReport().setRefuseDescription(bean.getRefuseMotivation());
+			updateReportStatus(bean.getCompanyReport(), "reporRefused");
+     		dao.update(bean.getCompanyReport());
+
 //			bean.getCompanyReport().notify();
-//			Stage stage = (Stage) form.submitRefuseButton.getScene().getWindow();
-//			stage.close();
-//		} else {
-//			dao.delete(bean.getCompanyReport().getIdReport());
-//			Stage stage = (Stage) form.submitRefuseButton.getScene().getWindow();
-//			stage.close();
-//		}
+			Stage stage = (Stage) AcceptOrRefuseJobForm.submitRefuseButton.getScene().getWindow();
+			stage.close();
+		} else {
+		 dao.delete(bean.getCompanyReport().getIdReport());
+			Stage stage = (Stage) AcceptOrRefuseJobForm.submitRefuseButton.getScene().getWindow();
+			stage.close();
+		}
 
 	}
 
@@ -87,28 +87,6 @@ public class AcceptOrRefuseAJobController {
 
 	}
 
-	@SuppressWarnings("null")
-	public void updateView(String view) throws IOException {
-		if (view.compareTo("accept") == 1) {
-
-			Stage primaryStage = null;
-			Parent root = FXMLLoader.load(getClass().getResource("accept.fxml"));
-			Scene scene = new Scene(root, 400, 400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-
-		}
-
-		if (view.compareTo("refuse") == 1) {
-			Stage primaryStage = null;
-			Parent root = FXMLLoader.load(getClass().getResource("refuse.fxml"));
-			Scene scene = new Scene(root, 400, 400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		}
-
-	}
+	
 
 }
--- fixmycity/src/main/java/com/ispw/fixmycity/logic/view/javafx/AcceptOrRefuseJobForm.java
+++ fixmycity/src/main/java/com/ispw/fixmycity/logic/view/javafx/AcceptOrRefuseJobForm.java
@@ -3,12 +3,8 @@ package com.ispw.fixmycity.logic.view.javafx;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import com.ispw.fixmycity.logic.app.App;
 import com.ispw.fixmycity.logic.bean.AcceptOrRefuseJobBean;
 import com.ispw.fixmycity.logic.controller.AcceptOrRefuseAJobController;
 import com.ispw.fixmycity.logic.dao.CompanyReportDAO;
@@ -22,7 +18,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.DialogPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
@@ -38,6 +33,13 @@ public class AcceptOrRefuseJobForm implements Observer  {
 
 	@FXML
 	private ImageView reportImageView;
+	@FXML
+	public static Button backButton;
+	@FXML
+	
+	public static Button submitAcceptButton;
+	@FXML
+	public static  Button submitRefuseButton;
 
 	@FXML
 	private Text reportTitleText;
@@ -47,12 +49,6 @@ public class AcceptOrRefuseJobForm implements Observer  {
 
 	@FXML
 	private DatePicker endDatePicker;
-
-	@FXML
-	private DialogPane acceptDialog;
-
-	@FXML
-	private DialogPane refuseDialog;
 
 	@FXML
 	private TextArea refuseDescription;
@@ -78,27 +74,21 @@ public class AcceptOrRefuseJobForm implements Observer  {
 	private TextArea status;
 
 	private CompanyReport reportSelected;
+	private CompanyReport report;
 	
 	AcceptOrRefuseJobBean bean = new AcceptOrRefuseJobBean();
 	AcceptOrRefuseAJobController controller = new AcceptOrRefuseAJobController();
 	CompanyReportDAO dao = new CompanyReportDAO();
 	//List<CompanyReport> reports = dao.findAll();
-	public List<Integer> observableReportId = new ArrayList<>();
+	//public List<Integer> observableReportId = new ArrayList<>();
 	
 
 	
-	@SuppressWarnings("null")
+	
 	@FXML
 	public void initialize() {
 		
-		List<CompanyReport> reports = null;
-		for(int i=0;i<observableReportId.size();i++) {
-			reports.add(dao.findByPrimaryKey(observableReportId.get(i)));
-		}
-		//CompanyReportDAO dao = new CompanyReportDAO();
-	//	List<CompanyReport> reports = dao.findAll();
-		acceptDialog.setVisible(false);
-		refuseDialog.setVisible(false);
+		List<CompanyReport> reports = dao.findAll();
 		final TreeItem<CompanyReport> root = new TreeItem<>();
 		reportTable.setRoot(root);
 		reportTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
@@ -114,25 +104,32 @@ public class AcceptOrRefuseJobForm implements Observer  {
 		reports.stream().forEach((report) -> {
 			root.getChildren().add(new TreeItem<>(report));
 		});
-
+		
+       
 		statusColumn.setCellValueFactory(
 				(TreeTableColumn.CellDataFeatures<CompanyReport, String> param) -> new ReadOnlyStringWrapper(
 						param.getValue().getValue().getStatus()));
-
+        
 		titleColumn.setCellValueFactory(
 				(TreeTableColumn.CellDataFeatures<CompanyReport, String> param) -> new ReadOnlyStringWrapper(
 						param.getValue().getValue().getTitle()));
 		
 	}
-
 	@FXML
 	public void acceptJob() throws IOException {
 		CompanyReport report=this.reportSelected;
-		acceptDialog.setVisible(true);
 		bean.setIdJob(report.getIdReport());
 		bean.setRelatedReport(report);
 	    bean.setRelatedCompany(report.getCompanyUser());
-	    controller.updateView("accept");}
+			Stage primaryStage = null;
+			Parent root = FXMLLoader.load(getClass().getResource("accept.fxml"));
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		}
+	   
 		
 
 	
@@ -144,17 +141,24 @@ public class AcceptOrRefuseJobForm implements Observer  {
 	@FXML
 	public void refuseJob() throws IOException {
 		CompanyReport report=this.reportSelected;
-		refuseDialog.setVisible(true);
 		bean.setIdJob(report.getIdReport());
 		bean.setRelatedReport(report);
 	    bean.setRelatedCompany(report.getCompanyUser());
-	    controller.updateView("refuse");
+			Stage primaryStage = null;
+			Parent root = FXMLLoader.load(getClass().getResource("refuse.fxml"));
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+	    
 
-	}
+	
 
 	@FXML
 	public void submitRefuse() {
 		bean.setRefuseMotivation(refuseDescription);
+		
 		 controller.refuseReport( bean);
 
 	}
@@ -172,16 +176,10 @@ public class AcceptOrRefuseJobForm implements Observer  {
 	
       @Override
       public void update() {
-    	 initialize();//refresh windows}
+    	// initialize();//refresh windows}
       }
     	 
-    	@FXML
-    	public Button backButton;
-    	@FXML
-		
-    	public Button submitAcceptButton;
-    	@FXML
-    	public Button submitRefuseButton;
+    	
       @SuppressWarnings("null")
 	@FXML
       public void backButton(ActionEvent event) {
